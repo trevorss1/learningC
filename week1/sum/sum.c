ab5b5cafb33fec67439fa80cab6e54c910b9c6e7
Trevor <trevorhywong@gmail.com>
2023-09-21 00:09:20 +1000

    I am confused now, feels like i should not be adding an int input in int main()

--- week1/sum/sum.c
+++ week1/sum/sum.c
@@ -1,25 +1,57 @@
 #include<stdio.h>
 
+// int main()
+//{
+    //declare x
+  //  int x, s, y, a;
+    //calculate 
+  //  do 
+  //  {
+  //      printf("enter the integer: ");
+  //      scanf("%d", &x);
+   // }
+  //  while (x < 0);
+    
+    
+ //   for (s = 0; s <= x; s++)
+  //  {
+  //      y = y + s; 
+  //  } 
+  //    printf("%d\n", y);
+      
+  //  return 0;
+//}
+
+
+int sum_to(int a)
+{
+    int x;
+    do 
+        {
+            printf("enter the integer: ");
+            scanf("%d", &x);
+        }
+    while ( x < 0);
+
+    for (int s = 0; s <=  x; s++)
+    {
+        a = a + s;
+    }
+    return a;
+}
 int main()
 {
-    //declare x
-    int x, s, y, a;
-    //calculate 
+    int x;
     do 
+        {
+            printf("enter the integer: ");
+            scanf("%d", &x);
+        }
+    while ( x < 0);
+    
     {
-        printf("enter the integer: ");
-        scanf("%d", &x);
+        int y = sum_to(x);
+        printf("%d\n", y);
+        return 0;
     }
-    while (x < 0);
-    
-    
-    for (s = 0; s <= x; s++)
-    {
-        y = y + s; 
-    } 
-      printf("%d\n", y);
-      
-    return 0;
 }
-
-
