17208b56814abb9090041a22b89d17a667dd57e3
Trevor <trevorhywong@gmail.com>
2023-09-20 11:08:38 +1000

    attempt for loop with y=y+1

--- week1/sum/sum.c
+++ week1/sum/sum.c
@@ -1,17 +1,27 @@
 #include<stdio.h>
 
-int main()
+int main(s)
 {
     //declare x
-    int x;
-    
-    //input x
-    printf("enter the integer: ");
-    scanf("%d", &x);
-
-    //calculate and display x
-    printf("%d\n", x*(x+1)/2);
-    
-    return 0;   
+    int x, s, y, a;
+    //calculate 
+    do 
+    {
+        printf("enter the integer: ");
+        scanf("%d", &x);
+    }
+    while (x < 0);
+    {
+    do (int s = 0; s < x; s++)
+    {
+        int y += s;
+    }
+    while (s < x);
+    }
+    {
+        printf("%d", y);
+    }
+    return 0
 }
-    
+
+
